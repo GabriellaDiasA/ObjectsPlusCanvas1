594f2afd3b3b06e968b575496bf3b592a79be9e1
GabriellaDiasA <gabriella.dias.abdo@gmail.com>
2021-01-10 12:10:01 -0300

    commit nao gosta de varios sinais de exclamacao juntos. USE LIVE SERVER OU NAO FUNCIONA

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import { canvas, c } from "./canvas.js";
 import { circulo } from "./circulo.js";
 import "./events.js";
 
+
+// Módulos não funcionam em arquivos locais por causa de uma tal de CORS policy. Use Live Server pelo VSCode!
 function animate(){
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
